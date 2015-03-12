74a49100c9291d7273e950326ffb062da8414626
arkaprava08 <arka@Arkapravas-MacBook-Air.local>
2015-03-13 02:07:53 +0530

    insert data 1

--- application/controllers/api/health.php
+++ application/controllers/api/health.php
@@ -3,28 +3,34 @@
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
 require APPPATH.'/libraries/REST_Controller.php';
 
+ini_set('display_errors', 'On');
+
 class Health extends REST_Controller
 {
 	function __construct()
     {
-        // Construct our parent class
         parent::__construct();
         
         $this->load->database();
         $this->load->library('session');
-
-        // Configure limits on our controller methods. Ensure
-        // you have created the 'limits' table and enabled 'limits'
-        // within application/config/rest.php
-        //$this->methods['user_get']['limit'] = 500; //500 requests per hour per user/key
-        //$this->methods['user_post']['limit'] = 100; //100 requests per hour per user/key
-        //$this->methods['user_delete']['limit'] = 50; //50 requests per hour per user/key
     }
     
+    public function test_get()
+    {
+        echo "testing";
+    }
+
+    //check if loggedin or not
+    public function isLoggedIn()
+    {
+        if($this->session->userdata('loggedin'))
+            return true;
+
+        return false;
+    }
     //login service
     public function login_post()
     {
-        //var_dump($this->request->body);
 
         if(isset($this->request->body['username']) && isset($this->request->body['password']))
         {
@@ -43,10 +49,12 @@ class Health extends REST_Controller
 
             if($query->num_rows() == 1)
             {
-                $this->response(array('userid' => $userid),200);
 
                 $this->session->set_userdata('userid', $userid);
-                $this->session->set_userdata('loggedin', 'true');
+                $this->session->set_userdata('loggedin', true);
+
+                $this->response(array('userid' => $userid),200);
+
 
                 //print_r($this->session->all_userdata()) ;
             }
@@ -66,22 +74,138 @@ class Health extends REST_Controller
     //logout service
     public function logout_get()
     {
+
         
-        
-        if (isset($this->session)) {
+        if ($this->isLoggedIn()) {
 
-            $this->session->set_userdata('loggedin', 'false');
+            $this->session->set_userdata('loggedin', false);
 
-            $this->response($this->session->all_userdata()) ;
+            $this->session->sess_destroy();
+
+            //$this->response($this->session->all_userdata()) ;
 
 
 
-            //$this->response(array('error' => 'Successfully Logged Out'),200);
+            $this->response(array('error' => 'Successfully Logged Out'),200);
         }
         else
         {
             $this->response(array('error' => 'No session exists to logout'),400);
         }
         
+    }
+
+    //insert data
+    public function insert_post()
+    {
+        if(!$this->isLoggedIn())
+        {
+            $this->response(array('error' => 'User not logged in '),401);   
+        }
+
+        
+        //get userid from session
+        if($this->session->userdata('userid'))
+            $userid = $this->session->userdata('userid');
+        else
+            $this->response(array('error' => 'User not logged in '),401); 
+
+
+
+        //if id of patient is passed
+        if(isset($this->request->body['id']))
+        {
+            if(isset($this->request->body['patientid']) 
+                && isset($this->request->body['bodytemperature'])
+                && isset($this->request->body['bloodpressure'])
+                && isset($this->request->body['symptoms'])
+                && isset($this->request->body['comment']))
+            {
+
+                $patientid = $this->request->body['patientid'];
+                $bodytemperature = $this->request->body['bodytemperature'];
+                $bloodpressure = $this->request->body['bloodpressure'];
+                $symptoms = $this->request->body['symptoms'];
+                $comment = $this->request->body['comment'];
+
+            }
+            else
+            {
+                $this->response(array('error' => 'Data not provided'),400); 
+            }
+        }
+        //else id is not passed i.e. new data
+        else
+        {
+            if (isset($this->request->body['name']) 
+                && isset($this->request->body['age'])
+                && isset($this->request->body['gender'])
+                && isset($this->request->body['address'])
+                && isset($this->request->body['bodytemperature'])
+                && isset($this->request->body['bloodpressure'])
+                && isset($this->request->body['symptoms'])
+                && isset($this->request->body['comment'])) {
+
+
+                $name = $this->request->body['name'];
+                $age = $this->request->body['age'];
+                $gender = $this->request->body['gender'];
+                $address = $this->request->body['address'];
+                $bodytemperature = $this->request->body['bodytemperature'];
+                $bloodpressure = $this->request->body['bloodpressure'];
+                $symptoms = $this->request->body['symptoms'];
+                $comment = $this->request->body['comment'];
+
+                $patientid;
+
+                $checkDataRedundancy = "SELECT `id` FROM `patients` WHERE  name ='"
+                                        .$name."' and age =".$age." and gender='"
+                                        .$gender."' and address='".$address."' and userid=".$userid;
+
+                $query = $this->db->query($checkDataRedundancy);
+
+                if($query->num_rows() > 0)
+                {
+                        $this->response(array('error' => 'Data already exists'),400); 
+                }
+                else
+                {
+                    $this->db->trans_start();
+
+                    $insertData = "INSERT INTO `patients`( `name`, `age`, `gender`, `address`, `userid`) "
+                        ."VALUES ('".$name."', ".$age.",'".$gender."','".$address."',".$userid.");";
+
+                    $query = $this->db->query($insertData);
+
+                    $getUniqueId = "SELECT `id` FROM `patients` WHERE  name ='".$name."' and age =".$age." and gender='".$gender."' and address='".$address."' and userid=".$userid;
+
+                    $query = $this->db->query($getUniqueId);
+
+                    foreach ($query->result() as $row)
+                    {
+                        $patientid = $row->id;
+                    }
+                    //we have $patientid
+                    $sql = "INSERT INTO `patientdata`( `patientid`, `userid`, `bodytemperature`, `bloodpressure`, `symptoms`, `comment`, `insertedDate`) "
+                                                ."VALUES (".$patientid.",".$userid.",".$bodytemperature.",".$bloodpressure.",'".$symptoms."','".$comment."', NOW())";
+
+
+                    $query = $this->db->query($sql);
+
+                    $this->db->trans_complete();
+                }
+
+                
+            }
+            else
+            {
+                $this->response(array('error' => 'Data not provided'),400); 
+            }
+        }
+
+
+
+
+
     }
 }
