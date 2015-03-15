5469ee4e15ebf71e76c0e47a7fb4caf431001b4b
arkaprava08 <arka@Arkapravas-MacBook-Air.local>
2015-03-16 01:43:04 +0530

    table new

--- table.sql
+++ table.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Mar 12, 2015 at 09:46 PM
+-- Generation Time: Mar 15, 2015 at 09:12 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.6.2
 
@@ -25,11 +25,24 @@ CREATE TABLE `patientdata` (
   `patientid` int(12) NOT NULL,
   `userid` int(12) NOT NULL,
   `bodytemperature` int(4) NOT NULL,
-  `bloodpressure` int(4) NOT NULL,
-  `symptoms` varchar(30) NOT NULL,
-  `comment` varchar(100) NOT NULL,
+  `bp_sp` int(4) NOT NULL,
+  `bp_dp` int(4) NOT NULL,
+  `symptoms` varchar(30) DEFAULT NULL,
+  `comment` varchar(100) DEFAULT NULL,
+  `latitude` decimal(9,0) DEFAULT NULL,
+  `longitude` decimal(9,0) DEFAULT NULL,
   `insertedDate` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `patientdata`
+--
+
+INSERT INTO `patientdata` (`id`, `patientid`, `userid`, `bodytemperature`, `bp_sp`, `bp_dp`, `symptoms`, `comment`, `latitude`, `longitude`, `insertedDate`) VALUES
+(7, 1, 1, 91, 78, 0, '1,2,3,4', 'DSAADSDAS', NULL, NULL, '2015-03-15 18:10:09'),
+(8, 1, 1, 91, 78, 0, '1,2,3,4', 'DSAADSDAS', NULL, NULL, '2015-03-15 18:10:11'),
+(13, 6, 1, 91, 78, 0, '3', 'DSAADSDAS', NULL, NULL, '2015-03-15 19:24:49'),
+(19, 12, 1, 91, 78, 78, '3', 'DSAADSDAS', NULL, NULL, '2015-03-15 23:29:32');
 
 -- --------------------------------------------------------
 
@@ -44,14 +57,16 @@ CREATE TABLE `patients` (
   `gender` varchar(1) NOT NULL,
   `address` varchar(100) NOT NULL,
   `userid` int(12) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `patients`
 --
 
 INSERT INTO `patients` (`id`, `name`, `age`, `gender`, `address`, `userid`) VALUES
-(1, 'das', 21, 'm', 'dsa', 1);
+(1, 'das', 21, 'm', 'dsa', 1),
+(6, 'arka', 21, 'M', 'asdasdasdas', 1),
+(12, 'arka1', 21, 'M', 'asdasdasdas', 1);
 
 -- --------------------------------------------------------
 
@@ -104,12 +119,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `patientdata`
 --
 ALTER TABLE `patientdata`
-MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT for table `patients`
 --
 ALTER TABLE `patients`
-MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
+MODIFY `id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `users`
 --
